835a4b9f2c498a795d181e561cfcd30ce114c710
khs2810 <khs_2810@naver.com>
2024-03-21 19:00:18 +0900

    no message

---
+++ resources/js/1.hoisting.js
@@ -0,0 +1,21 @@
+/**
+ * hoisting
+ */
+
+console.log(name1); // name
+var name1 = "hyeonseo";
+console.log(name1);
+
+
+
+/**
+ * 
+ * Hoisting은 뭘까?
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상
+ * let, const
+ */
+
+
+console.log(name2);
+let name2 = "hyeonseo";
---
+++ resources/js/2.scope.js
@@ -0,0 +1,52 @@
+/**
+ * Scope
+ */
+
+var number1 = 20; // window가 가지고 있음
+
+function test1() {
+    console.log(number1);
+}
+
+// test1();
+
+function test2() {
+    var number1 = 40;
+    console.log(number1);
+}
+//scope가 가지고 있음
+
+// test2();
+
+// 변수는 두번 선언했지만, 함수 안에서 먼저 찾고, 함수안에 없다면, 그 밖에서 찾는다.
+
+var number1 = 20;
+function test3() {
+    var number1 = 40;
+    test4();
+    console.log("number1 : " + number1)
+}
+
+function test4() {
+    var number2 = 11;
+    console.log("number2 : " + number2)
+    console.log("number1 : " + number1)
+}
+
+test3();
+console.log(number1);
+
+/**
+ * JS => Lexical Scope
+ * 선언된 위치가 상위 스코프를 정한다.
+ * 
+ * 
+ * Dynamic Scope
+ * 실행한 위치가 상위 스코프를 정한다.
+ */
+
+var i = 1000;
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log("i = " + i);

f3c7e57b5bc6f26659f7f20cbc0f7c742d3e4915
khs2810 <khs_2810@naver.com>
2024-03-22 16:11:00 +0900

    ui구현

--- resources/js/2.scope.js
+++ resources/js/2.scope.js
@@ -45,8 +45,14 @@ console.log(number1);
  * 실행한 위치가 상위 스코프를 정한다.
  */
 
+// var i = 1000;
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// console.log("i = " + i);
+
 var i = 1000;
-for (var i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
     console.log(i);
 }
 console.log("i = " + i);
